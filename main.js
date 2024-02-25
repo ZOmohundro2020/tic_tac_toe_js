f852365174a822cca1f5590eaf1ff91f90b53ee3
ZOmohundro <zaneomohundro@gmail.com>
2024-02-25 10:31:29 -0500

    restructure the event listener to use FormData object

--- main.js
+++ main.js
@@ -3,16 +3,27 @@ const book_popup_close = document.querySelector("book_form_close");
 const bookForm = document.getElementById("bookForm");
 const bookFormElements = bookForm.elements;
 
+//play with this
+// const fd = new FormData(event.target);
+// let inputFieldData = fd.get("cityInput");
+
+// if (inputFieldData.trim().length < 1) {
+//   return;
+// }
+
 bookForm.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("submitting");
-  console.log(bookFormElements["b-title"].value);
+  const fd = new FormData(event.target);
+  let bTitle = fd.get("b-title");
+  let bAuthor = fd.get("b-author");
+  let bPages = fd.get("b-pages");
+  let bRead = fd.get("b-read");
 
   const book = new Book({
-    title: bookFormElements["b-title"].value,
-    author: bookFormElements["b-title"].value,
-    pages: bookFormElements["b-title"].value,
-    read: bookFormElements["b-title"].value
+    title: bTitle,
+    author: bAuthor,
+    pages: bPages,
+    read: bRead,
   });
   myLibrary.push(book);
   console.log(myLibrary);
@@ -20,7 +31,7 @@ bookForm.addEventListener("submit", (event) => {
 
 let myLibrary = [];
 
-function Book({title, author, pages, read}) {
+function Book({ title, author, pages, read }) {
   this.title = title;
   this.author = author;
   this.pages = pages;
