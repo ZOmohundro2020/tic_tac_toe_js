c16597c9a8454e58202b3e9ac8a139fddc0f3496
ZOmohundro <zaneomohundro@gmail.com>
2024-03-03 22:02:26 -0500

    rough in active player toggle

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 function GameBoard() {
-  const gameBoard = [
-    ["X", "X", "X"],
-    ["X", "X", "X"],
-    ["X", "X", "X"],
+  let gameBoard = [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"],
   ];
-  return { gameBoard };
+  const placePiece = (row, col, piece) => {
+    console.log(row, col, piece);
+    gameBoard[row][col] = piece;
+  };
+  return { gameBoard, placePiece };
 }
 
 function Player(playerName, playerPiece, activePlayer = false) {
@@ -16,12 +20,46 @@ function GameControl() {
   const board = GameBoard();
   const PlayerX = Player("Bob", "X", true);
   const PlayerY = Player("Jim", "O");
-  const displayBoard = console.log(board);
-  console.log(PlayerX);
-  console.log(PlayerY);
+  const players = [PlayerX, PlayerY];
+  const displayBoard = () => {
+    console.log(board.gameBoard);
+  };
+  displayBoard();
 
-  const turnControl = function () {};
-  //display board changes
+  const turnControl = function () {
+    // determine active player
+    const determineActivePlayer = function () {
+      let activePlayer;
+      players.map((player) => {
+        if (player.activePlayer) {
+          activePlayer = player;
+        }
+      });
+
+      console.log("activePlayer is: ", activePlayer);
+      return activePlayer;
+    };
+
+    const toggleActivePlayer = function () {
+      players.map((player) => (player.activePlayer = !player.activePlayer));
+      console.log("players array is now: ", players);
+    };
+
+    // flow control
+    activePlayer = determineActivePlayer();
+
+    //sample first turn
+    console.log(
+      `It is ${activePlayer.playerName}'s turn and they are playing as ${activePlayer.playerPiece} `
+    );
+    board.placePiece(0, 0, activePlayer.playerPiece);
+    displayBoard();
+    toggleActivePlayer();
+    console.log("after first turn: ", activePlayer.playerName);
+  };
+  //console.log("Enter Row,Col coordinates");
+
+  turnControl();
 }
 
 GameControl();
